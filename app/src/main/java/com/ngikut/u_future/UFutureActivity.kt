c70fe71d199d48d77f64d99d5e48e9f3c8e55018
Adri x Fahmi <fahmigutawan@gmail.com>
2023-04-23 12:05:44 +0700

    Add color

--- app/src/main/java/com/ngikut/u_future/UFutureActivity.kt
+++ app/src/main/java/com/ngikut/u_future/UFutureActivity.kt
@@ -4,11 +4,6 @@ import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import com.ngikut.u_future.ui.theme.UFutureTheme
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
 
