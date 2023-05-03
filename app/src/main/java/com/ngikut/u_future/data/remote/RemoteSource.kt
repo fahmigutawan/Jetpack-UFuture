ae6fd4f71417fed6c49105d0b5cfc29b85356e78
fahmirumagutawan <fahmigutawan@gmail.com>
2023-05-03 13:17:13 +0700

    Login API been finished to be an example to other API call

--- app/src/main/java/com/ngikut/u_future/data/remote/RemoteSource.kt
+++ app/src/main/java/com/ngikut/u_future/data/remote/RemoteSource.kt
@@ -1,9 +1,15 @@
 package com.ngikut.u_future.data.remote
 
+import com.ngikut.u_future.model.remote.request.student.LoginRequest
+import com.ngikut.u_future.model.remote.response.student.LoginResponse
 import io.ktor.client.*
+import io.ktor.client.call.body
 import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.plugins.RedirectResponseException
 import io.ktor.client.plugins.ServerResponseException
+import io.ktor.client.request.post
+import io.ktor.client.request.url
+import io.ktor.client.statement.HttpResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -13,11 +19,26 @@ import javax.inject.Inject
 class RemoteSource @Inject constructor(
     private val client:HttpClient
 ) {
-    private fun <T> getResponse(
-        httpCall: suspend () -> ApiResponse<T>
+    fun login(
+        request:LoginRequest
+    ):Flow<ApiResponse<LoginResponse>> = getResponse {
+        client.post {
+            url(HttpEndpoint.LOGIN)
+        }
+    }
+
+    private inline fun <reified T> getResponse(
+        crossinline httpCall: suspend () -> HttpResponse
     ): Flow<ApiResponse<T>> = flow {
         try {
-            emit(httpCall())
+            val res = httpCall()
+
+            if(res.status.value.toString()[0] == '2'){
+                emit(ApiResponse.Success(res.body<T>()))
+            }else{
+                emit(ApiResponse.Error("Terjadi kesalahan saat mendapatkan data"))
+            }
+
         } catch (e: RedirectResponseException) {
             // 3xx - responses
             emit(ApiResponse.Error("Error: ${e.response.status.description}"))
--- app/src/main/java/com/ngikut/u_future/data/repository/Repository.kt
+++ app/src/main/java/com/ngikut/u_future/data/repository/Repository.kt
@@ -1,7 +1,14 @@
 package com.ngikut.u_future.data.repository
 
 import com.ngikut.u_future.data.datastore.DatastoreSource
+import com.ngikut.u_future.data.remote.ApiResponse
 import com.ngikut.u_future.data.remote.RemoteSource
+import com.ngikut.u_future.data.remote.Resource
+import com.ngikut.u_future.model.remote.request.student.LoginRequest
+import com.ngikut.u_future.model.remote.response.student.LoginResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class Repository @Inject constructor(
@@ -15,4 +22,18 @@ class Repository @Inject constructor(
     suspend fun setToken(token:String) = datastoreSource.setToken(token)
 
     fun getToken() = datastoreSource.getToken()
+
+    fun login(email:String, password:String): Flow<Resource<LoginResponse>> = flow{
+        when(val res = remoteSource.login(LoginRequest(email, password)).first()){
+            ApiResponse.Empty -> {
+                emit(Resource.Error(""))
+            }
+            is ApiResponse.Error -> {
+                emit(Resource.Error(res.errorMessage))
+            }
+            is ApiResponse.Success -> {
+                emit(Resource.Success(res.data))
+            }
+        }
+    }
 }
